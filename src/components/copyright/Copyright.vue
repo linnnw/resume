8c5106b7358fa1cd59866c7bc4b35b73a6074f73
Lin <1666455972@qq.com>
2019-10-11 15:10:15 +0800

    1

--- src/components/copyright/Copyright.vue
+++ src/components/copyright/Copyright.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="copyright">
-        <p>Copyright © 2019 林伟 All Rights Reserved</p>
+        <p>Copyright © 2019 林伟 All Rights Reserved <br>闽ICP备19020203号</p>
     </div>
 </template>
 
@@ -18,14 +18,27 @@
         position: relative;
     }
     p {
-        width: 350px;
+        width: 450px;
         text-align: center;
-        font-size: 18px;
+        font-size: 14px;
 
         position: absolute;
         left: 50%;
         top: 50%;
         transform: translate(-50%,-50%);
         color: #fff;
+    }
+    @media screen and (max-width: 767px){
+        p {
+            width: 300px;
+            text-align: center;
+            font-size: 14px;
+
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%,-50%);
+            color: #fff;
+        }
     }
 </style>
